e4ed4b1ed6cc00a9615c080e5907abf5a88a96a7
moondev03 <moondev03@gmail.com>
2024-07-26 02:43:50 +0900

    [ dependency ] Add LoggerUtil | https://github.com/orhanobut/logger

--- ThinkerBell/app/build.gradle.kts
+++ ThinkerBell/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
@@ -33,6 +34,11 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+        buildConfig = true
+    }
 }
 
 dependencies {
@@ -45,4 +51,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.logger)
 }
