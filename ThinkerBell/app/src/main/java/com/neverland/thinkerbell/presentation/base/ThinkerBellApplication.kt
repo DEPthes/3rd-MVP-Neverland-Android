0ddd6ed0d969a58872d8e01212ae8a8a77575543
moondev03 <moondev03@gmail.com>
2024-07-26 03:03:22 +0900

    [ feat ] Add ThinkerBellApplication

---
+++ ThinkerBell/app/src/main/java/com/neverland/thinkerbell/presentation/base/ThinkerBellApplication.kt
@@ -0,0 +1,10 @@
+package com.neverland.thinkerbell.presentation.base
+
+import android.app.Application
+
+class ThinkerBellApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
