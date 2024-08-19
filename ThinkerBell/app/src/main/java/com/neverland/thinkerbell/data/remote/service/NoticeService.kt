fd9b32880234091c226d6534f4dd99b51b2321df
moondev03 <moondev03@gmail.com>
2024-08-19 23:08:21 +0900

    [ feat ] Add APIs Notice

--- ThinkerBell/app/src/main/java/com/neverland/thinkerbell/data/remote/service/NoticeService.kt
+++ ThinkerBell/app/src/main/java/com/neverland/thinkerbell/data/remote/service/NoticeService.kt
@@ -1,29 +1,104 @@
 package com.neverland.thinkerbell.data.remote.service
 
 import com.neverland.thinkerbell.data.remote.model.BaseResponse
-import com.neverland.thinkerbell.data.remote.model.notice.DormitoryNoticeDTO
+import com.neverland.thinkerbell.data.remote.model.PageableResponse
+import com.neverland.thinkerbell.data.remote.model.notice.CommonNoticeDTO
 import com.neverland.thinkerbell.data.remote.model.notice.JobTrainingNoticeDTO
-import com.neverland.thinkerbell.data.remote.model.notice.LibraryNoticeDTO
-import com.neverland.thinkerbell.data.remote.model.notice.TeachingNoticeDTO
-import com.neverland.thinkerbell.data.remote.model.univ.DeptContactResDTO
-import com.neverland.thinkerbell.data.remote.model.univ.DeptUrlResDTO
+import com.neverland.thinkerbell.data.remote.model.notice.SearchNoticeResultDTO
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface NoticeService {
 
+    @GET("/api/notices/search")
+    suspend fun searchNotices(
+        @Query("keyword") keyword: String,
+        @Query("ssaid") ssaId: String
+    ): Response<BaseResponse<SearchNoticeResultDTO>>
+
     @GET("/api/dormitory/notices")
-    suspend fun getDormitoryNotices(): Response<BaseResponse<List<DormitoryNoticeDTO>>>
+    suspend fun getDormitoryNotices(
+        @Query("page") page: Int,
+        @Query("ssaid") ssaId: String
+    ): Response<BaseResponse<PageableResponse<CommonNoticeDTO>>>
 
     @GET("/api/dormitory/entry-notices")
-    suspend fun getDormitoryEntryNotices(): Response<BaseResponse<List<DormitoryNoticeDTO>>>
+    suspend fun getDormitoryEntryNotices(
+        @Query("page") page: Int,
+        @Query("ssaid") ssaId: String
+    ): Response<BaseResponse<PageableResponse<CommonNoticeDTO>>>
 
     @GET("/api/job-training")
-    suspend fun getJobTrainingNotices(): Response<BaseResponse<List<JobTrainingNoticeDTO>>>
+    suspend fun getJobTrainingNotices(
+        @Query("page") page: Int,
+        @Query("ssaid") ssaId: String
+    ): Response<BaseResponse<PageableResponse<JobTrainingNoticeDTO>>>
 
     @GET("/api/library")
-    suspend fun getLibraryNotices(): Response<BaseResponse<List<LibraryNoticeDTO>>>
+    suspend fun getLibraryNotices(
+        @Query("page") page: Int,
+        @Query("ssaid") ssaId: String
+    ): Response<BaseResponse<PageableResponse<CommonNoticeDTO>>>
 
+    // 학교 공지
     @GET("/api/teaching")
-    suspend fun getTeachingNotices(): Response<BaseResponse<List<TeachingNoticeDTO>>>
+    suspend fun getTeachingNotices(
+        @Query("page") page: Int,
+        @Query("ssaid") ssaId: String
+    ): Response<BaseResponse<PageableResponse<CommonNoticeDTO>>>
+
+    @GET("/api/academic")
+    suspend fun getAcademicNotices(
+        @Query("page") page: Int,
+        @Query("ssaid") ssaId: String
+    ): Response<BaseResponse<PageableResponse<CommonNoticeDTO>>>
+
+    @GET("/api/event")
+    suspend fun getEventNotices(
+        @Query("page") page: Int,
+        @Query("ssaid") ssaId: String
+    ): Response<BaseResponse<PageableResponse<CommonNoticeDTO>>>
+
+    @GET("/api/normal")
+    suspend fun getNormalNotices(
+        @Query("page") page: Int,
+        @Query("ssaid") ssaId: String
+    ): Response<BaseResponse<PageableResponse<CommonNoticeDTO>>>
+
+    @GET("/api/revision")
+    suspend fun getRevisionNotices(
+        @Query("page") page: Int,
+        @Query("ssaid") ssaId: String
+    ): Response<BaseResponse<PageableResponse<CommonNoticeDTO>>>
+
+    @GET("/api/safety")
+    suspend fun getSafetyNotices(
+        @Query("page") page: Int,
+        @Query("ssaid") ssaId: String
+    ): Response<BaseResponse<PageableResponse<CommonNoticeDTO>>>
+
+    @GET("/api/scholarship")
+    suspend fun getScholarship(
+        @Query("page") page: Int,
+        @Query("ssaid") ssaId: String
+    ): Response<BaseResponse<PageableResponse<CommonNoticeDTO>>>
+
+    @GET("/api/student-acts")
+    suspend fun getStudentActsNotices(
+        @Query("page") page: Int,
+        @Query("ssaid") ssaId: String
+    ): Response<BaseResponse<PageableResponse<CommonNoticeDTO>>>
+
+    @GET("/api/career")
+    suspend fun getCareerNotices(
+        @Query("page") page: Int,
+        @Query("ssaid") ssaId: String
+    ): Response<BaseResponse<PageableResponse<CommonNoticeDTO>>>
+
+    @GET("/api/bidding")
+    suspend fun getBiddingNotices(
+        @Query("page") page: Int,
+        @Query("ssaid") ssaId: String
+    ): Response<BaseResponse<PageableResponse<CommonNoticeDTO>>>
 }
--- ThinkerBell/app/src/main/java/com/neverland/thinkerbell/domain/repository/NoticeRepository.kt
+++ ThinkerBell/app/src/main/java/com/neverland/thinkerbell/domain/repository/NoticeRepository.kt
@@ -1,16 +1,38 @@
 package com.neverland.thinkerbell.domain.repository
 
+import com.neverland.thinkerbell.domain.enums.NoticeType
+import com.neverland.thinkerbell.domain.model.PageableNotice
 import com.neverland.thinkerbell.domain.model.notice.NoticeItem
 
 interface NoticeRepository {
 
-    suspend fun getDormitoryNotices(): Result<List<NoticeItem.CommonNotice>>
+    suspend fun getDormitoryNotices(page: Int, ssaId: String): Result<PageableNotice<NoticeItem.CommonNotice>>
 
-    suspend fun getDormitoryEntryNotices(): Result<List<NoticeItem.CommonNotice>>
+    suspend fun getDormitoryEntryNotices(page: Int, ssaId: String): Result<PageableNotice<NoticeItem.CommonNotice>>
 
-    suspend fun getJobTrainingNotices(): Result<List<NoticeItem.JobNotice>>
+    suspend fun getJobTrainingNotices(page: Int, ssaId: String): Result<PageableNotice<NoticeItem.JobNotice>>
 
-    suspend fun getLibraryNotices(): Result<List<NoticeItem.CommonNotice>>
+    suspend fun getLibraryNotices(page: Int, ssaId: String): Result<PageableNotice<NoticeItem.CommonNotice>>
 
-    suspend fun getTeachingNotices(): Result<List<NoticeItem.CommonNotice>>
+    suspend fun getTeachingNotices(page: Int, ssaId: String): Result<PageableNotice<NoticeItem.CommonNotice>>
+
+    suspend fun getAcademicNotices(page: Int, ssaId: String): Result<PageableNotice<NoticeItem.CommonNotice>>
+
+    suspend fun getEventNotices(page: Int, ssaId: String): Result<PageableNotice<NoticeItem.CommonNotice>>
+
+    suspend fun getNormalNotices(page: Int, ssaId: String): Result<PageableNotice<NoticeItem.CommonNotice>>
+
+    suspend fun getRevisionNotices(page: Int, ssaId: String): Result<PageableNotice<NoticeItem.CommonNotice>>
+
+    suspend fun getSafetyNotices(page: Int, ssaId: String): Result<PageableNotice<NoticeItem.CommonNotice>>
+
+    suspend fun getScholarshipNotices(page: Int, ssaId: String): Result<PageableNotice<NoticeItem.CommonNotice>>
+
+    suspend fun getStudentActsNotices(page: Int, ssaId: String): Result<PageableNotice<NoticeItem.CommonNotice>>
+
+    suspend fun getCareerNotices(page: Int, ssaId: String): Result<PageableNotice<NoticeItem.CommonNotice>>
+
+    suspend fun getBiddingNotices(page: Int, ssaId: String): Result<PageableNotice<NoticeItem.CommonNotice>>
+
+    suspend fun searchNoticesByCategory(noticeType: NoticeType, keyword: String, ssaId: String): Result<List<NoticeItem>>
 }
