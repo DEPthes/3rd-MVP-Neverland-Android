abff84f6017f0895b55e68a8d10408451bd5713f
moondev03 <moondev03@gmail.com>
2024-08-20 03:40:45 +0900

    [ feat ] Update Response Banner | Add Notice Url

--- ThinkerBell/app/src/main/java/com/neverland/thinkerbell/data/remote/model/univ/BannerResDTO.kt
+++ ThinkerBell/app/src/main/java/com/neverland/thinkerbell/data/remote/model/univ/BannerResDTO.kt
@@ -5,5 +5,6 @@ import com.google.gson.annotations.SerializedName
 data class BannerResDTO(
     @SerializedName("id") val id: Int,
     @SerializedName("title") val title: String,
-    @SerializedName("s3Url") val s3Url: String
+    @SerializedName("s3Url") val s3Url: String,
+    @SerializedName("noticeUrl") val noticeUrl: String
 )
--- ThinkerBell/app/src/main/java/com/neverland/thinkerbell/domain/model/univ/Banner.kt
+++ ThinkerBell/app/src/main/java/com/neverland/thinkerbell/domain/model/univ/Banner.kt
@@ -3,5 +3,6 @@ package com.neverland.thinkerbell.domain.model.univ
 data class Banner (
     val id: Int,
     val title: String,
-    val s3Url: String
+    val s3Url: String,
+    val noticeUrl: String
 )
