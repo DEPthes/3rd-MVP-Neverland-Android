c1d1aa019db8a28f54dbadc11ae14ef4df7bd9c6
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2024-08-20 03:51:23 +0900

    [ feat ] 즐겨찾기한 공지사항 어댑터 구현

--- ThinkerBell/app/src/main/java/com/neverland/thinkerbell/presentation/view/myPage/adapter/FavoriteVPAdapter.kt
+++ ThinkerBell/app/src/main/java/com/neverland/thinkerbell/presentation/view/myPage/adapter/FavoriteVPAdapter.kt
@@ -2,13 +2,18 @@ package com.neverland.thinkerbell.presentation.view.myPage.adapter
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.neverland.thinkerbell.domain.enums.NoticeType
+import com.neverland.thinkerbell.domain.model.notice.BookmarkNotice
+import com.neverland.thinkerbell.domain.model.notice.NoticeItem
 import com.neverland.thinkerbell.presentation.view.myPage.FavoriteNoticeFragment
 
-class FavoriteVPAdapter(fragment: Fragment, private val category: List<String>) : FragmentStateAdapter(fragment) {
+class FavoriteVPAdapter(fragment: Fragment, private val category: Map<NoticeType, List<NoticeItem>>) : FragmentStateAdapter(fragment) {
 
     override fun getItemCount(): Int = category.size
 
     override fun createFragment(position: Int): Fragment {
-        return FavoriteNoticeFragment.newInstance(category[position])
+        val key = category.keys.toList()[position]
+        return FavoriteNoticeFragment(category[key] ?: emptyList(), NoticeType.entries.find { it.enName==key.enName }!!)
     }
+
 }
