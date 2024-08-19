4d672f9ab141fd23a7ce89aa29613e79e18a64d8
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2024-08-20 04:58:21 +0900

    [ refactor ] SerializedName 수정

--- ThinkerBell/app/src/main/java/com/neverland/thinkerbell/data/remote/model/notice/BookmarkNoticeDTO.kt
+++ ThinkerBell/app/src/main/java/com/neverland/thinkerbell/data/remote/model/notice/BookmarkNoticeDTO.kt
@@ -3,17 +3,17 @@ package com.neverland.thinkerbell.data.remote.model.notice
 import com.google.gson.annotations.SerializedName
 
 data class BookmarkNoticeDTO(
-    @SerializedName("safetyNotice") val safetyNotice: List<CommonNoticeDTO>?,
-    @SerializedName("revisionNotice") val revisionNotice: List<CommonNoticeDTO>?,
-    @SerializedName("libraryNotice") val libraryNotice: List<CommonNoticeDTO>?,
-    @SerializedName("dormitoryNotice") val dormitoryNotice: List<CommonNoticeDTO>?,
-    @SerializedName("teachingNotice") val teachingNotice: List<CommonNoticeDTO>?,
-    @SerializedName("jobTrainingNotice") val jobTrainingNotice: List<JobTrainingNoticeDTO>?,
-    @SerializedName("eventNotice") val eventNotice: List<CommonNoticeDTO>?,
-    @SerializedName("studentActsNotice") val studentActsNotice: List<CommonNoticeDTO>?,
-    @SerializedName("academicNotice") val academicNotice: List<CommonNoticeDTO>?,
-    @SerializedName("careerNotice") val careerNotice: List<CommonNoticeDTO>?,
-    @SerializedName("biddingNotice") val biddingNotice: List<CommonNoticeDTO>?,
-    @SerializedName("dormitoryEntryNotice") val dormitoryEntryNotice: List<CommonNoticeDTO>?,
-    @SerializedName("scholarshipNotice") val scholarshipNotice: List<CommonNoticeDTO>?
+    @SerializedName("SafetyNotice") val safetyNotice: List<CommonNoticeDTO>?,
+    @SerializedName("RevisionNotice") val revisionNotice: List<CommonNoticeDTO>?,
+    @SerializedName("LibraryNotice") val libraryNotice: List<CommonNoticeDTO>?,
+    @SerializedName("DormitoryNotice") val dormitoryNotice: List<CommonNoticeDTO>?,
+    @SerializedName("TeachingNotice") val teachingNotice: List<CommonNoticeDTO>?,
+    @SerializedName("JobTrainingNotice") val jobTrainingNotice: List<JobTrainingNoticeDTO>?,
+    @SerializedName("EventNotice") val eventNotice: List<CommonNoticeDTO>?,
+    @SerializedName("StudentActsNotice") val studentActsNotice: List<CommonNoticeDTO>?,
+    @SerializedName("AcademicNotice") val academicNotice: List<CommonNoticeDTO>?,
+    @SerializedName("CareerNotice") val careerNotice: List<CommonNoticeDTO>?,
+    @SerializedName("BiddingNotice") val biddingNotice: List<CommonNoticeDTO>?,
+    @SerializedName("DormitoryEntryNotice") val dormitoryEntryNotice: List<CommonNoticeDTO>?,
+    @SerializedName("ScholarshipNotice") val scholarshipNotice: List<CommonNoticeDTO>?
 )
